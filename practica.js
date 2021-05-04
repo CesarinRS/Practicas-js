4fa857124f3c9c0d16b88def8d343431b882740a
julesc00 <julesc00@hotmail.com>
2021-05-04 13:45:16 -0500

    info personal

--- practica.js
+++ practica.js
@@ -1,3 +1,37 @@
-const nombre = "Cesarito"
+function Persona(nombre, apellido, estatura, colorPelo, padres) {
+  this.nombre = nombre;
+  this.apellido = apellido;
+  this.edad = 17;
+  this.estatura = estatura;
+  this.colorPelo = colorPelo;
+  this.padres = padres;
 
-console.log(nombre);
+  this.imprimirPersona = function () {
+    return (
+      this.nombre +
+      " " +
+      this.apellido +
+      "(" +
+      this.edad +
+      ")" +
+      " estatura: " +
+      this.estatura +
+      "," +
+      " Color de pelo " +
+      this.colorPelo +
+      "," +
+      " hijo de " +
+      this.padres
+    );
+  };
+}
+
+const cesar = new Persona(
+  "Cesar",
+  "Briones",
+  "1.75",
+  "Castaño",
+  "Julio Cesar y Claudia Huerta"
+);
+
+console.log(cesar.imprimirPersona());
